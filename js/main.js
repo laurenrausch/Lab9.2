8b947822f6c87c30fab0b3df71951f29795b1b01
Lauren Rausch <laurenrausch447@u.boisestate.edu>
2025-03-12 23:46:54 -0600

    evilcircle changes

--- js/main.js
+++ js/main.js
@@ -19,10 +19,12 @@ function randomRGB() {
 }
 
 class Ball extends Shape {
+  exists;
   constructor(x, y, velX, velY, color, size) {
     super(x, y, velX, velY);
     this.color = color;
     this.size = size;
+    this.exists = true;
   }
 
   draw() {
@@ -54,14 +56,16 @@ class Ball extends Shape {
   }
 
   collisionDetect() {
-    for (const ball of balls) {
-      if (!(this === ball)) {
-        const dx = this.x - ball.x;
-        const dy = this.y - ball.y;
-        const distance = Math.sqrt(dx * dx + dy * dy);
+    if (exists == true) {
+      for (const ball of balls) {
+        if (!(this === ball)) {
+          const dx = this.x - ball.x;
+          const dy = this.y - ball.y;
+          const distance = Math.sqrt(dx * dx + dy * dy);
 
-        if (distance < this.size + ball.size) {
-          ball.color = this.color = randomRGB();
+          if (distance < this.size + ball.size) {
+            ball.color = this.color = randomRGB();
+          }
         }
       }
     }
@@ -74,6 +78,30 @@ class Shape {
     this.y = y;
     this.velX = velX;
     this.velY = velY;
+  }
+}
+
+class EvilCircle extends Shape {
+  constructor(x, y) {
+    super(x, y, 20, 20);
+    this.color = "white";
+    this.size = 10;
+    window.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "a":
+          this.x -= this.velX;
+          break;
+        case "d":
+          this.x += this.velX;
+          break;
+        case "w":
+          this.y -= this.velY;
+          break;
+        case "s":
+          this.y += this.velY;
+          break;
+      }
+    });
   }
 }
 
